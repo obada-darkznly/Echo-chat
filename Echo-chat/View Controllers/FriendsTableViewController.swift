78aa2fc9b589f2f98bf9b3d05e31add241ae1eaf
Obada <obada_darkznly@hotmail.com>
2020-08-20 12:18:05 +0300

    Created chat list VC

---
+++ Echo-chat/View Controllers/FriendsTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FriendsTableViewController.swift
+//  Echo-chat
+//
+//  Created by Obada on 8/20/20.
+//  Copyright © 2020 Obada. All rights reserved.
+//
+
+import UIKit
+
+
+class FriendsTableViewController: UITableViewController {
+    
+    // MARK: Properties
+    
+    
+    // MARK: Controller's life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+    }
+}
