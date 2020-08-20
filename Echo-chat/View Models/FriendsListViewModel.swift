3d5ee97b92829a135c8bdc459e3ba3a3bc73ce9e
Obada <obada_darkznly@hotmail.com>
2020-08-20 14:51:07 +0300

    Created the friends list view model

---
+++ Echo-chat/View Models/FriendsListViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsListViewModel.swift
+//  Echo-chat
+//
+//  Created by Obada on 8/20/20.
+//  Copyright © 2020 Obada. All rights reserved.
+//
+
+import Foundation
+
+
+struct FriendsListViewModel {
+    
+    // MARK: Properties
+    
+    // MARK: Methods
+}
