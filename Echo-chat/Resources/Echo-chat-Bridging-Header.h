f49eb938d4930cf8033ec4d2724d4efdd06b6785
Obada <obada_darkznly@hotmail.com>
2020-08-21 20:52:39 +0300

    Added an obj-c bridging header

---
+++ Echo-chat/Resources/Echo-chat-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Echo-chat-Bridging-Header.h
+//  Echo-chat
+//
+//  Created by Obada on 8/21/20.
+//  Copyright © 2020 Obada. All rights reserved.
+//
+
+#ifndef Echo_chat_Bridging_Header_h
+#define Echo_chat_Bridging_Header_h
+
+#import <UIScrollView_InfiniteScroll/UIScrollView+InfiniteScroll.h>
+
+#endif /* Echo_chat_Bridging_Header_h */
