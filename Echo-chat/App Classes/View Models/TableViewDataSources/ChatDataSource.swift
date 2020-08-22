858f97e13235581463f4731e52fd49ad170a3169
Obada <obada_darkznly@hotmail.com>
2020-08-22 14:51:58 +0300

    Created the chat data source

---
+++ Echo-chat/App Classes/View Models/TableViewDataSources/ChatDataSource.swift
@@ -0,0 +1,36 @@
+//
+//  ChatDataSource.swift
+//  Echo-chat
+//
+//  Created by Obada on 8/22/20.
+//  Copyright © 2020 Obada. All rights reserved.
+//
+
+import UIKit
+
+
+class ChatDataSource: NSObject {
+    
+    let messages: [Message]
+    
+    init(withMessages messages: [Message]) {
+        self.messages = messages
+    }
+}
+
+extension ChatDataSource: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "chatListCellId") as? ChatListCell else { return UITableViewCell() }
+        cell.populate(withMessageText: messages[indexPath.row].text ?? "")
+        return cell
+    }
+}
